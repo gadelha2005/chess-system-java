2feb863731bebee5871be7be862f0425c484e406
Pedro Gadelha <pgpgadelha123@gmail.com>
2024-06-27 10:01:17 -0300

    Cretead project

---
+++ src/application/App.java
@@ -0,0 +1,19 @@
+package application;
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+	    Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Hello World!");
+
+
+	    sc.close();
+	}
+        
+    }
+
