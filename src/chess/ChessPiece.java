1ae7dbd8b8827d752d579258609b9a2a2d9277c2
Pedro Gadelha <pgpgadelha123@gmail.com>
2024-07-01 09:14:07 -0300

    Review2

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -5,18 +5,5 @@ import boardgame.Piece;
 import chess.Color;
 
 public class ChessPiece extends Piece {
-    private Color color;
-
-    public ChessPiece(){
-        super();
-    }
-
-    public ChessPiece(Board board , Color color){
-        super(board);
-        this.color = color;
-    }
-
-    public Color getColor(){
-        return color;
-    }
+  
 }
